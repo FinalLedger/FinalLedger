f771e2082452bf8a1a1f33e54a19f41cd718f131
JMComstock <jasoncomstock11@gmail.com>
2022-05-02 16:11:55 -0500

    initial commit

---
+++ src/main/java/com/finalledger/FinalLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.finalledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finalledger/FinalLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
