caf3ed86565948a24faf523fa5862682ed771dbe
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-05-03 10:14:43 -0500

    created register html

--- src/main/java/com/finalledger/controllers/UserController.java
+++ src/main/java/com/finalledger/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.finalledger.controllers;
 
 import com.finalledger.models.User;
 import com.finalledger.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private final UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userDao) {
-
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register")
-    public String showSignupForm(Model model){
+    public String showRegistrationForm(Model model){
         model.addAttribute("user", new User());
-        return "users/sign-up";
+        return "/register";
     }
 
     @PostMapping("/register")
