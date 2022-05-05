0bc2456207573e2c4640f4bbb09b9596770d5bf1
Therman116220 <thermanhaynes@gmail.com>
2022-05-05 16:06:55 -0500

    update

--- src/main/java/com/finalledger/controllers/AuthenticationController.java
+++ src/main/java/com/finalledger/controllers/AuthenticationController.java
@@ -1,12 +1,23 @@
 package com.finalledger.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
+
+    @Value("${googleAPI}")
+    public String googleAPIKey;
+
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model model) {
+
+        model.addAttribute("googleAPI", googleAPIKey);
+
         return "users/login";
     }
 }
+
+
