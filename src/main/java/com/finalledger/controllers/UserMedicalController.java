9de0a4bdee160a799508a5d6efb3f5a236d1363a
ranchayasambath <ranchayasambath@gmail.com>
2022-05-05 12:13:28 -0500

    corrected controller annotations errors

--- src/main/java/com/finalledger/controllers/UserMedicalController.java
+++ src/main/java/com/finalledger/controllers/UserMedicalController.java
@@ -1,18 +1,20 @@
 package com.finalledger.controllers;
 
 import com.finalledger.models.UserMedicalInformation;
+import com.finalledger.repositories.UserMedicalRepository;
 import com.finalledger.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Controller
 public class UserMedicalController {
     private final UserRepository userDao;
-    private final UserMedicalController userMedicalDao ;
+    private final UserMedicalRepository userMedicalDao ;
 
-    public UserMedicalController(UserRepository userDao, UserMedicalController userMedicalDao) {
+    public UserMedicalController(UserRepository userDao, UserMedicalRepository userMedicalDao) {
         this.userDao = userDao;
         this.userMedicalDao = userMedicalDao;
     }
@@ -20,7 +22,7 @@ public class UserMedicalController {
     @GetMapping("/ledger/medical")
     public String showUserMedicalForm(Model model){
         model.addAttribute("medical", new UserMedicalInformation());
-        return("/ledger/medical");
+        return("ledger/medical");
     }
 
     @PostMapping("/ledger/medical")
