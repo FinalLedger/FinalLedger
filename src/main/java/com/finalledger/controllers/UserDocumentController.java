421a6f3b8fc3ae1f906350e768b7de292e1644bf
ranchayasambath <ranchayasambath@gmail.com>
2022-05-06 14:56:15 -0500

    updated index page

--- src/main/java/com/finalledger/controllers/UserDocumentController.java
+++ src/main/java/com/finalledger/controllers/UserDocumentController.java
@@ -1,42 +1,42 @@
-package com.finalledger.controllers;
-
-import com.finalledger.models.UserDocuments;
-import com.finalledger.repositories.UserDocumentsRepository;
-import com.finalledger.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class UserDocumentController {
-
-    @Value("${fileStackAPI}")
-    public String fileStackAPIKey;
-
-    private final UserRepository userDao;
-    private final UserDocumentsRepository userDocumentsDao;
-
-    public UserDocumentController(UserRepository userDao, UserDocumentsRepository userDocumentsDao) {
-        this.userDao = userDao;
-        this.userDocumentsDao = userDocumentsDao;
-    }
-
-    @GetMapping("/ledger/documents")
-    public String showUserDocuments(Model model){
-
-        model.addAttribute("documents", new UserDocuments());
-        model.addAttribute("fileStackAPI", fileStackAPIKey);
-
-        return "ledger/documents";
-    }
-
-    @PostMapping("/ledger/documents")
-    public String saveUserDocuments(@ModelAttribute UserDocuments userDocuments){
-
-        return("redirect:/ledger/documents");
-    }
-
-}
+//package com.finalledger.controllers;
+//
+//import com.finalledger.models.UserDocuments;
+//import com.finalledger.repositories.UserDocumentsRepository;
+//import com.finalledger.repositories.UserRepository;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class UserDocumentController {
+//
+//    @Value("${fileStackAPI}")
+//    public String fileStackAPIKey;
+//
+//    private final UserRepository userDao;
+//    private final UserDocumentsRepository userDocumentsDao;
+//
+//    public UserDocumentController(UserRepository userDao, UserDocumentsRepository userDocumentsDao) {
+//        this.userDao = userDao;
+//        this.userDocumentsDao = userDocumentsDao;
+//    }
+//
+//    @GetMapping("/ledger/documents")
+//    public String showUserDocuments(Model model){
+//
+//        model.addAttribute("documents", new UserDocuments());
+//        model.addAttribute("fileStackAPI", fileStackAPIKey);
+//
+//        return "ledger/documents";
+//    }
+//
+//    @PostMapping("/ledger/documents")
+//    public String saveUserDocuments(@ModelAttribute UserDocuments userDocuments){
+//
+//        return("redirect:/ledger/documents");
+//    }
+//
+//}
