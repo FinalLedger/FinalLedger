c0b9d830ea4d59288e633231dd5694599f607465
JMComstock <jasoncomstock11@gmail.com>
2022-05-10 15:09:18 -0500

    added policy controller and started a partials footer

---
+++ src/main/java/com/finalledger/controllers/PolicyController.java
@@ -0,0 +1,39 @@
+package com.finalledger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PolicyController {
+
+    @GetMapping("/policies/cookies")
+    public String showCookies() {
+
+        return "policies/cookies";
+    }
+
+    @GetMapping("/policies/disclaimer")
+    public String showDisclaimer() {
+
+        return "policies/disclaimer";
+    }
+
+    @GetMapping("/policies/eula")
+    public String showEULA() {
+
+        return "policies/eula";
+    }
+
+    @GetMapping("/policies/privacy")
+    public String showPrivacy() {
+
+        return "policies/privacy";
+    }
+
+    @GetMapping("/policies/terms")
+    public String showTerms() {
+
+        return "policies/terms";
+    }
+
+}
