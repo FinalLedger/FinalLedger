362f01397e8bac823236185957b39a0020fa5ff2
JMComstock <jasoncomstock11@gmail.com>
2022-05-12 16:59:56 -0500

    changed the structre of the files to make the code more readable

--- src/main/java/com/finalledger/FinalLedgerApplication.java
+++ src/main/java/com/finalledger/FinalLedgerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinalLedgerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FinalLedgerApplication.class, args);
     }
 
