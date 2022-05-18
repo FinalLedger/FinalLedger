794d8eecb3cea85a09b535bd086c266674ac0bfa
ranchayasambath <ranchayasambath@gmail.com>
2022-05-18 14:44:50 -0500

    updated changes

--- src/main/java/com/finalledger/controllers/AuthenticationController.java
+++ src/main/java/com/finalledger/controllers/AuthenticationController.java
@@ -1,9 +1,8 @@
 package com.finalledger.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
