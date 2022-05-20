931ae0651662505d3a7f512e38b829393345ec52
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-05-20 00:33:13 -0500

    fixed select values on forms, got rid of isMainUser on User, added profile pics

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,5 +1,29 @@
-$(document).ready(function () {
+window.addEventListener("DOMContentLoaded", function () {
     // $("#submitBtn").attr("disabled", "disabled");
+
+    let apikey = document.querySelector('meta').content;
+    const client = filestack.init(apikey); // filestack is not defined
+
+    const options = {
+        onUploadDone: (res) => {
+            $("#profilePicUrl").val(res.filesUploaded[0].url);
+            console.log(res);
+        },
+        accept: ["image/*"],
+        uploadConfig: {
+            retry: 5,
+            timeout: 60000
+        },
+        transformations: {
+            crop: true,
+            circle: true,
+            rotate: true
+        }
+    }
+
+    $("#buttonUpload").click(function () {
+        client.picker(options).open();
+    })
 
     $("#password").keyup(function () {
         let password = $("#password").val();
@@ -81,5 +105,4 @@ $(document).ready(function () {
     //         $("#submitBtn").attr("disabled", "disabled");
     //     }
     // })
-
 })
