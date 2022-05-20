75542b9ed637f06576a451e5ea280f956ef0f1fb
JMComstock <jasoncomstock11@gmail.com>
2022-05-20 00:00:31 -0500

    added new searchByEmail to User repository, added more search code to profile.html, added searchEmail to controller

--- src/main/java/com/finalledger/repositories/UserRepository.java
+++ src/main/java/com/finalledger/repositories/UserRepository.java
@@ -2,6 +2,11 @@ package com.finalledger.repositories;
 
 import com.finalledger.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -9,7 +14,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findByEmail(String email);
 
-
     User getUserByUsername(String username);
     User getUserById(long id);
+
+    @Query(value = "SELECT * FROM final_ledger_db.users WHERE email LIKE %?1%", nativeQuery = true)
+    List<User> searchByEmail(@Param("email") String email);
 }
